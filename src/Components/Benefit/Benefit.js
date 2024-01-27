1e635d5c63c6cbf74fdf1b42bbe32021b56dfb80
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-01-27 03:21:32 +0100

    yes

--- src/Components/Benefit/Benefit.js
+++ src/Components/Benefit/Benefit.js
@@ -6,6 +6,56 @@ function Benefit() {
     <div className='benefit'>
          <span className='feat'>BENEFITS</span>
          <h2>Why Choose SmartWave as Your Affiliate Partner?</h2>
+
+         <div className='ben-con'>
+            <div className='ben-box'>
+             <div className='ben-icon'>
+              <img src="https://res.cloudinary.com/dxnukbo0u/image/upload/v1706321775/icons8-jet-85_q3ekaa.png" alt="" />
+             </div>
+             <p>Proven Success</p>
+             <span>Join a community where over 20,000 affiliates are already flourishing and over 500 vendors have found unprecedented success.</span>
+            </div>
+
+            <div className='ben-box'>
+             <div className='ben-icon'>
+              <img src="https://res.cloudinary.com/dxnukbo0u/image/upload/v1706321775/icons8-money-50_na1zld.png" alt="" />
+             </div>
+             <p>Lucrative Earning Potential</p>
+             <span> Our affiliates consistently make over 500,000 Naira a month – a testament to the extraordinary earning potential within the SmartWave ecosystem.</span>
+            </div>
+
+            <div className='ben-box'>
+             <div className='ben-icon'>
+              <img src="https://res.cloudinary.com/dxnukbo0u/image/upload/v1706321775/icons8-digital-64_m22ace.png" alt="" />
+             </div>
+             <p>Digital Diversity</p>
+             <span>Access a vast array of digital products, ensuring that there's something for every niche. Your audience is diverse, and so are your earning opportunities.</span>
+            </div>
+
+            <div className='ben-box'>
+             <div className='ben-icon'>
+              <img src="https://res.cloudinary.com/dxnukbo0u/image/upload/v1706321775/icons8-reward-50_rdlgg1.png" alt="" />
+             </div>
+             <p>Holistic Rewards</p>
+             <span> Beyond financial gains, SmartWave offers exclusive bonuses, incentives, and public recognition to celebrate your achievements.</span>
+            </div>
+
+            <div className='ben-box'>
+             <div className='ben-icon'>
+              <img src="https://res.cloudinary.com/dxnukbo0u/image/upload/v1706319001/icons8-team-50_i0hmdx.png" alt="" />
+             </div>
+             <p>Community Strength</p>
+             <span> Join a network of like-minded individuals, share experiences, and learn from the best in the business. Together, we grow stronger.</span>
+            </div>
+
+            <div className='ben-box'>
+             <div className='ben-icon'>
+              <img src="https://res.cloudinary.com/dxnukbo0u/image/upload/v1706319001/icons8-lock-50_yqgjem.png" alt="" />
+             </div>
+             <p>Security Assured</p>
+             <span>Your data and transactions are our priority. Trust SmartWave for a secure and reliable platform.</span>
+            </div>
+         </div>
     </div>
   )
 }

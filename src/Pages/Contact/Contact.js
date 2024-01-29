50931cfc3836f52ed1d2edbf05e533bca1918c42
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-01-29 05:11:57 +0100

    yes

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Contact.css';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+     <div className='about-hero'>
+        <h2>Contact Us</h2>
+        <p>Reach out to us anytime!</p>
+
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+        <div className='flake'></div>
+      </div>
+
+    </div>
   )
 }
 
